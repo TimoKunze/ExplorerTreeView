41e8740b02b0e43e60fdc188bdb74e97ba36393c
Timo Kunze <tkunze71216@gmx.de>
2018-02-04 01:06:34 +0100

    Prepared release of version 2.6.5.

--- src/res/resource.h
+++ src/res/resource.h
@@ -8,12 +8,12 @@
 
 #define VERSION_MAJOR																2
 #define VERSION_MINOR																6
-#define VERSION_REVISION1														4
+#define VERSION_REVISION1														5
 #define VERSION_REVISION2														0
-#define VERSION_BUILD																450
-#define STRFILEVERSION															"2.6.4.450"
+#define VERSION_BUILD																453
+#define STRFILEVERSION															"2.6.5.453"
 #define STRPRODUCTVERSION														"2.6"
-#define STRTIMESTAMP																"2018/01/29 22:12:06"
+#define STRTIMESTAMP																"2018/02/04 01:01:55"
 
 #define IDB_EXPLORERTREEVIEW												1
 #define IDB_ABOUT																		2
